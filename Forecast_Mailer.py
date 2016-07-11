36d907f04f5915623f563d6c3ffa8f51f84b3303
Edward J.E Koch <visceral910@gmail.com>
2016-07-11 05:07:09 -0700

    Loops through all users, emails them, and defines a schedule.

--- Forecast_Mailer.py
+++ Forecast_Mailer.py
@@ -60,16 +60,22 @@ def send_emails(emails, schedule, forecast):
 
     # Login
     password = raw_input("What is your password?")
-    from_email = "my_email@gmail.com"
-    to_email = "your_email.com"
+    from_email = "from_email@gmail.com"
+    to_email = "to_email@gmail.com"
     server.login(from_email, password)
-    
-    #Send email
-    message = "Subject: Seattle forecast using Python\n"
-    message += "What's up Josh. I am testing a forecast mailer using Python,\n"
-    message += "which retrieves a weather API and sends an e-mail to people on my list.\n\n "
-    message += "This entire e-mail is being generated using Python code.\n\n"
-    message += "(cue forecast variable!)\n\n" + forecast
+
+    # Send to entire email list
+    for to_email, name in emails.items():
+        message = "Subject: Forecast mailer using Python\n"
+        message += "What's up " + name + ", I am testing a forecast mailer using Python,\n"
+        message += "which retrieves a weather API and sends an e-mail to people on my list.\n\n "
+        message += "This entire e-mail is being generated using Python code.\n\n"
+        message += "(cue forecast variable!)\n\n" + forecast + "\n \n"
+        message += "The following code also generates a schedule: \n\n" + schedule
+        server.sendmail(from_email, to_email, message)
+            
+
+ 
    
             
     server.sendmail(from_email, to_email, message)
